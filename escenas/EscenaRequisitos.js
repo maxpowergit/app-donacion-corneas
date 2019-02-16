a280fc99f923e8bbd4c15c5e4a483b005f6b1c04
Max <maxpower@riseup.net>
2019-02-16 13:11:36 -0300

    Agregadas preguntas para requisitos

--- escenas/EscenaRequisitos.js
+++ escenas/EscenaRequisitos.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Container, Content } from 'native-base'
 
 import PreguntaRequisitos from '../componentes/PreguntaRequisitos.js'
 
 export default class EscenaRequisitos extends Component {
   render() {
     return (
-      <PreguntaRequisitos
-        pregunta= '¿La edad es entre los 2 y los 80 años?'
-      />   
+      <Container>
+        <View flex= { 1 } style= {estilos.contenedorPreguntas}>
+          <PreguntaRequisitos
+            pregunta= '¿La edad es entre los 2 y los 80 años?'
+          />
+          <PreguntaRequisitos
+            pregunta= '¿El tiempo de internación es menor a 4 semanas?'
+          />
+          <PreguntaRequisitos
+            pregunta= '¿Es conocida la causa de muerte?'
+          />
+          <PreguntaRequisitos
+            pregunta= '¿El PCR es hace menos de 6 horas, o 12 horas si el cadáver fue refrigerado dentro de la primera hora?'
+          />
+        </View>
+      </Container>
     )
   }
 }
+
+const estilos = StyleSheet.create({
+  contenedorPreguntas: {
+    justifyContent: 'space-around',
+    padding: 8,
+  },
+})
