c11564d5ca28b2e62a40da34931495d4a6ed9efc
Max <maxpower@riseup.net>
2019-03-23 23:14:33 -0300

    Fix de preguntas requisitos

--- src/lib/PreguntasRequisitos.js
+++ src/lib/PreguntasRequisitos.js
@@ -1,6 +1,6 @@
 // Listado de referencia con las preguntas de requisitos.
 
-const PreguntasRequisitos = [
+const PreguntasRequisitos = {
   edad: {
     llave: 'edad',
     pregunta: '¿La edad es entre los 2 y los 80 años?'
@@ -17,6 +17,6 @@ const PreguntasRequisitos = [
     llave: 'refrigerado',
     pregunta: '¿El PCR es hace menos de 6 horas, o 12 horas si el cadáver fue refrigerado dentro de la primera hora?'
   }
-]
+}
 
 export default PreguntasRequisitos
