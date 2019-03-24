bd749899932d9183295d1ed8062378408059d542
Max <maxpower@riseup.net>
2019-03-23 22:03:35 -0300

    Mapa de preguntas requisitos

---
+++ src/lib/PreguntasRequisitos.js
@@ -0,0 +1,22 @@
+// Listado de referencia con las preguntas de requisitos.
+
+const PreguntasRequisitos = [
+  edad: {
+    llave: 'edad',
+    pregunta: '¿La edad es entre los 2 y los 80 años?'
+  },
+  tiempo: {
+    llave: 'tiempo',
+    pregunta: '¿El tiempo de internación es menor a 4 semanas?'
+  },
+  causa: {
+    llave: 'causa',
+    pregunta: '¿Es conocida la causa de muerte?'
+  },
+  refrigerado: {
+    llave: 'refrigerado',
+    pregunta: '¿El PCR es hace menos de 6 horas, o 12 horas si el cadáver fue refrigerado dentro de la primera hora?'
+  }
+]
+
+export default PreguntasRequisitos
