0ea22ccfe4107df051b151f217daebf387a70fe4
Max <maxpower@riseup.net>
2019-05-09 11:42:59 -0300

    Arreglos de estilo escena Contacto

--- src/estilos/componentes/DatoDonante.js
+++ src/estilos/componentes/DatoDonante.js
@@ -1,11 +1,14 @@
 import { StyleSheet } from 'react-native'
+import { colores } from '../Base'
 
 export default StyleSheet.create({
   label: {
-    textTransform: 'uppercase'
+    textTransform: 'uppercase',
+    color: colores.terciario
   },
   // Ajustar inputs a la altura del texto.
   input: {
-    height: 0
+    height: 0,
+    color: colores.terciario
   }
 })
