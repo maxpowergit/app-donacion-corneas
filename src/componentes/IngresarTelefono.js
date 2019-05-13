c2728ef4d25f512f088ea05e44c5bf103086eeeb
Max <maxpower@riseup.net>
2019-05-12 22:52:43 -0300

    Arreglos estéticos a escena Ingreso

--- src/componentes/IngresarTelefono.js
+++ src/componentes/IngresarTelefono.js
@@ -3,34 +3,33 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { LayoutAnimation } from 'react-native'
-import { Item, Input, Label } from 'native-base'
+import { Item, Input, Text, View } from 'native-base'
 
 import estilos from '../estilos/componentes/IngresarTelefono'
 
 export default class IngresarTelefono extends Component {
-  // No estoy seguro de que esto haga algo.
   componentWillMount() {
     LayoutAnimation.easeInEaseOut()
   }
 
   render() {
-    const { guardarTelefono, autoFocus, telefono, floatingLabel } = this.props
+    const { guardarTelefono, autoFocus, telefono } = this.props
 
     return (
-      // Por default usamos floatingLabel (en el inicio) y si lo deshabilitamos
-      // usamos stackedLabel.
-      <Item floatingLabel={ floatingLabel } stackedLabel={ !floatingLabel } style={ estilos.item }>
-        <Label style={ estilos.label }>TELÉFONO DEL COORDINADOR HOSPITALARIO</Label>
-        <Input
-          value={ telefono }
-          style={ estilos.input }
-          selectTextOnFocus
-          onChange={ text => guardarTelefono(text.nativeEvent.text) }
-          keyboardType="number-pad"
-          autoFocus={ autoFocus }
-          onSubmitEditing={ event => guardarTelefono(event.nativeEvent.text) }
-        />
-      </Item>
+      <View style={ estilos.contenedor }>
+        <Text style={ estilos.label }>INGRESE EL TELÉFONO DE SU COORDINADOR HOSPITALARIO</Text>
+        <Item style={ estilos.item }>
+          <Input
+            value={ telefono }
+            style={ estilos.input }
+            selectTextOnFocus
+            onChange={ text => guardarTelefono(text.nativeEvent.text) }
+            keyboardType="number-pad"
+            autoFocus={ autoFocus }
+            onSubmitEditing={ event => guardarTelefono(event.nativeEvent.text) }
+          />
+        </Item>
+      </View>
     )
   }
 }
@@ -38,11 +37,9 @@ export default class IngresarTelefono extends Component {
 IngresarTelefono.propTypes = {
   guardarTelefono: PropTypes.func.isRequired,
   autoFocus: PropTypes.bool,
-  telefono: PropTypes.string.isRequired,
-  floatingLabel: PropTypes.bool
+  telefono: PropTypes.string.isRequired
 }
 
 IngresarTelefono.defaultProps = {
-  autoFocus: false,
-  floatingLabel: true
+  autoFocus: false
 }
--- src/estilos/componentes/IngresarTelefono.js
+++ src/estilos/componentes/IngresarTelefono.js
@@ -3,15 +3,23 @@ import { StyleSheet } from 'react-native'
 import { colores } from '../Base'
 
 export default StyleSheet.create({
+  contenedor: {
+    alignItems: 'center'
+  },
   item: {
-    margin: 14,
-    borderColor: colores.primario
+    borderColor: colores.primario,
+    borderBottomWidth: 1.5
   },
   label: {
     color: colores.primario,
-    fontFamily: 'Roboto_medium'
+    fontFamily: 'Roboto_medium',
+    fontWeight: 'bold',
+    textAlign: 'center'
   },
   input: {
-    color: colores.primario
+    color: colores.primario,
+    alignSelf: 'center',
+    marginLeft: 8,
+    marginRight: 8
   }
 })
