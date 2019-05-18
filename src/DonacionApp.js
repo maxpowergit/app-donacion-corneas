c0b9e82b388b1811feb8d2c6ebe0e9f248564e53
Max <maxpower@riseup.net>
2019-05-18 16:10:40 -0300

    Proyecto ejectado de expo y actualizado

--- src/DonacionApp.js
+++ src/DonacionApp.js
@@ -2,7 +2,6 @@
 import React, { Component } from 'react'
 import { StatusBar } from 'react-native'
 import { MenuProvider } from 'react-native-popup-menu'
-import { Font } from 'expo'
 
 import { colores } from './estilos/Base'
 import Navegador from './Navegador'
@@ -18,13 +17,6 @@ class DonacionApp extends Component {
   async componentDidMount() {
     // Deshabilitamos eslint porque esta es la forma recomendada de cargar
     // archivos tanto por eslint como por native-base.
-    await Font.loadAsync({
-      // eslint-disable-next-line global-require
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      // eslint-disable-next-line global-require
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf')
-    })
-
     this.setState({ loaded: true })
   }
 
