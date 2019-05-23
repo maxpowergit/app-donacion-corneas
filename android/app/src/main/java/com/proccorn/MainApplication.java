3fe14635c7e7be1d7a8190d0958c14d2bf4bce9a
Max <maxpower@riseup.net>
2019-05-23 17:15:35 -0300

    Agregada librería react-native-exit-app

--- android/app/src/main/java/com/proccorn/MainApplication.java
+++ android/app/src/main/java/com/proccorn/MainApplication.java
@@ -3,6 +3,7 @@ package com.proccorn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.github.wumke.RNExitApp.RNExitAppPackage;
 import com.tkporter.sendsms.SendSMSPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNExitAppPackage(),
             SendSMSPackage.getInstance(),
             new RNGestureHandlerPackage()
       );
