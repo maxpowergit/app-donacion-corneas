c0c9b3dc429c471355293a87f254a0efa5e8faa6
Max <maxpower@riseup.net>
2019-05-23 19:38:54 -0300

    Navegador parcialmente transformado en tabs

--- src/Navegador.js
+++ src/Navegador.js
@@ -1,5 +1,5 @@
 // Navegación principal, definición de escenas y rutas.
-import { createStackNavigator, createAppContainer } from 'react-navigation'
+import { createStackNavigator, createMaterialTopTabNavigator, createAppContainer } from 'react-navigation'
 
 import Ingreso from './escenas/Ingreso'
 import Requisitos from './escenas/Requisitos'
@@ -10,20 +10,35 @@ import Indicaciones from './escenas/Indicaciones'
 import Telefono from './escenas/Telefono'
 import AcercaDe from './escenas/AcercaDe'
 
-const Navegador = createStackNavigator({
+
+const NavegadorTabs = createMaterialTopTabNavigator({
   ingreso: { screen: Ingreso },
   requisitos: { screen: Requisitos },
   contraindicaciones: { screen: Contraindicaciones },
-  contraindicacion: { screen: Contraindicacion },
   contacto: { screen: Contacto },
   indicaciones: { screen: Indicaciones },
+}, {
+  initialRouteName: 'ingreso',
+  swipeEnabled: false,
+  backBehavior: 'order',
+  tabBarComponent: 'none',
+  defaultNavigationOptions: {
+    tabBarVisible: false
+  }
+})
+
+// un stack que tiene los tabs como primer escena.
+
+const NavegadorInicial = createStackNavigator({
+  tabs: { screen: NavegadorTabs },
+  contraindicacion: { screen: Contraindicacion },
   telefono: { screen: Telefono },
   acerca: { screen: AcercaDe }
 }, {
-  initialRouteName: 'ingreso',
+  initialRouteName: 'tabs',
   defaultNavigationOptions: {
     header: null
   }
 })
 
-export default createAppContainer(Navegador)
+export default createAppContainer(NavegadorInicial)
